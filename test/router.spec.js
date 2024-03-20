512e4c3c0c65c50e0de1ed8ad432e47be993f6e2
Evy-Oliveira <evelyn.o-s@hotmail.com>
2024-03-20 12:11:01 -0300

    Conclusão dos testes da onURLChange e navigateTo

--- test/router.spec.js
+++ test/router.spec.js
@@ -5,7 +5,6 @@ let Routes = {
   '/home': [() => {}],
   '/about': [() => {}],
 };
-
 describe('setRootEl', () => {
   it('Deve testar se a atribuição do elemento raiz foi bem-sucedida', () => {
     const rootElement = document.createElement('div');
@@ -47,14 +46,25 @@ describe('setRoutes', () => {
     expect(JSON.stringify(Routes)).toEqual(JSON.stringify(mockRoutes));
   });
 
-  it('deve lançar um erro se as rotas não forem um objeto', () => {
+  it('Deve testar se lança um erro se as rotas não forem um objeto', () => {
     expect(() => {
       setRoutes('não objeto');
     }).toThrow('Routes não é um objeto');
   });
 
-});
+  it('Deve testar se routes lança uma exceção', () => {
+    const mockRoutes = {
+      '/home': [() => {}],
+      '/about': [() => {}],
+    };
+   
+    expect(() => {
+      setRoutes(mockRoutes);
+    }).toThrow('Routes não definiu uma rota de erro');
 
+  });
+
+});
 describe('onURLChange', () =>{
   
   let mockLocation;
@@ -120,13 +130,39 @@ describe('navigateTo', () =>{
   afterEach(() => {
     mockPushState.mockRestore();
   });
-  it('Deve testar se atualiza o historico com pushState', () =>{
+  it('Deve testar se atualiza o historico com pushState e se funciona sem o envio do props', () =>{
+    const mockPathname = '/home';
+
+    navigateTo(mockPathname);
+
+    expect(mockPushState).toHaveBeenCalledWith({}, null, mockPathname);
+  });
+
+  it('Deve testar se recebe as props corretamente e se renderiza a pagina correta', () =>{
+    const mockRoutes = {
+      '/home': jest.fn((props) =>{
+        const viewHome = document.createElement('div');
+        viewHome.textContent = props['param1'];
+        return viewHome;
+      }), // Simula uma função para a rota '/home'
+      '/error': jest.fn(() =>{
+        const viewErro = document.createElement('div');
+        viewErro.textContent = 'erro';
+        return viewErro;
+      }), // Simula uma função para a rota '/error'
+    };
+    setRoutes(mockRoutes);
+
+    const rootElement = document.createElement('main');
+    setRootEl(rootElement);
+
     const mockPathname = '/home';
     const mockProps = {param1: 'value1', param2:'value2'};
 
     navigateTo(mockPathname, mockProps);
+    const div = rootElement.querySelector('div');
+    expect(div.textContent).toBe(mockProps['param1']);
 
-    expect(mockPushState).toHaveBeenCalledWith(mockProps, null, mockPathname);
   });
 
-})
+});
